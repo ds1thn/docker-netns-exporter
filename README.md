f045366d8b71b2e0bafd39fca55dc68e722705bf
Dmitry Paevsky <ds1thn@gmail.com>
2026-09-18 10:52:24 +0300

    Fix = summary metrics

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# docker_netns_tcp_state
+# docker-netns-exporter
 Number of TCP connections in a given state inside the Docker container's network namespace
 
 ## How to use
--- docker/app.sh
+++ docker/app.sh
@@ -1,53 +1,62 @@
 #!/bin/bash
 
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2025 Dmitry Paevsky
+# Copyright (c) 2026 Dmitry Paevsky
+
+set -uo pipefail
 
 CACHE_DIR="/tmp/docker_netns_cache"
-CACHE_TTL=60  # секунд
-
+CACHE_TTL=60
 METRICS_DIR=/app/metrics
 METRICS_FILE="$METRICS_DIR/metrics.prom"
+SOCK=/var/run/docker.sock
+API=http://localhost/v1.29
 
 mkdir -p "$CACHE_DIR" "$METRICS_DIR"
-
 CACHE_FILE="$CACHE_DIR/netns_map"
 NOW=$(date +%s)
 
-# Проверка TTL
 if [[ ! -f $CACHE_FILE || $(( NOW - $(stat -c %Y "$CACHE_FILE") )) -gt $CACHE_TTL ]]; then
-    echo "[INFO] Обновляю кэш docker netns → container name"
-    > "$CACHE_FILE"
-    # docker ps -q
-    for cid in $(curl -s --unix-socket /var/run/docker.sock http://localhost/v1.29/containers/json | jq -r '.[].Id'); do
-        # docker inspect --format '{{.NetworkSettings.SandboxKey}}' "$cid"
-        ns=$(curl -s --unix-socket /var/run/docker.sock http://localhost/v1.29/containers/$cid/json | jq -r '.NetworkSettings.SandboxKey')
-        # docker inspect --format '{{.Name}}' "$cid"
-        name=$(curl -s --unix-socket /var/run/docker.sock http://localhost/v1.29/containers/$cid/json | jq -r '.Name' | sed 's|^/||')
-        if [[ -n "$ns" && -n "$name" ]]; then
-            echo "${ns##*/} ${name#/}" >> "$CACHE_FILE"
-        fi
-    done
-else
-    echo "[INFO] Использую кэш"
+  : > "$CACHE_FILE"
+  for cid in $(curl -s --unix-socket "$SOCK" "$API/containers/json" | jq -r '.[].Id'); do
+    curl -s --unix-socket "$SOCK" "$API/containers/$cid/json" \
+    | jq -r '
+        select(.NetworkSettings.SandboxKey != null and .NetworkSettings.SandboxKey != "")
+        | [(.NetworkSettings.SandboxKey | split("/") | last),
+           (.Name | ltrimstr("/"))]
+        | @tsv' >> "$CACHE_FILE"
+  done
 fi
 
-# Формирование метрик
-> "$METRICS_FILE"
+declare -A ns2name
+while IFS=$'\t' read -r ns name; do
+  [[ -n "$ns" && -n "$name" ]] && ns2name["$ns"]="$name"
+done < "$CACHE_FILE"
 
-for i in $(ls -1 /var/run/docker/netns); do
-    cname=$(grep "^$i " "$CACHE_FILE" | awk '{print $2}')
-    if [[ -n "$cname" ]]; then
-        ns_path="/var/run/docker/netns/$i"
-        declare -A conn_states
-        while read -r state; do
-            ((conn_states["$state"]++))
-        done < <(nsenter --net="$ns_path" ss -tan | awk 'NR>1 {print $1}')
+TMP=$(mktemp "$METRICS_DIR/.metrics.XXXXXX")
+{
+  echo "# HELP docker_netns_tcp_state TCP sockets by state per container netns"
+  echo "# TYPE docker_netns_tcp_state gauge"
+} > "$TMP"
 
-        for state in "${!conn_states[@]}"; do
-            echo "docker_netns_tcp_state{container=\"${cname}\",state=\"${state}\"} ${conn_states[$state]}" >> "$METRICS_FILE"
-        done
-    fi
+for ns_path in /var/run/docker/netns/*; do
+  [[ -e "$ns_path" ]] || continue
+  i=${ns_path##*/}
+  cname=${ns2name[$i]:-}
+  [[ -n "$cname" ]] || continue
+
+  unset conn_states
+  declare -A conn_states
+
+  while read -r state; do
+    [[ -n "$state" ]] || continue
+    conn_states["$state"]=$(( ${conn_states["$state"]:-0} + 1 ))
+  done < <(nsenter --net="$ns_path" ss -tan 2>/dev/null | awk 'NR>1 {print $1}')
+
+  for state in "${!conn_states[@]}"; do
+    printf 'docker_netns_tcp_state{container="%s",state="%s"} %d\n' \
+           "$cname" "$state" "${conn_states[$state]}" >> "$TMP"
+  done
 done
 
-exit 0
+mv "$TMP" "$METRICS_FILE"
